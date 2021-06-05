60ddaef871b63d01128c6a9abaa921568171e89a
Life75 <austynwashington@yahoo.com>
2021-06-05 18:02:39 -0500

    starting calculator

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import "./App.css";
+
+
+export default App; //The name of the class that extends to component that you'll be displaying
