5a59601382ff673247c039368077fc9ee0f73b7e
Life75 <austynwashington@yahoo.com>
2021-06-07 11:56:13 -0500

    got the css working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
 import logo from "./logo.svg";
 import React, { Component } from "react";
-import "./App.css";
+import styles from "./App.css";
+
+class App extends Component {
+  
+    render() {
+        return (
+            <div className="page">
+            <CalculatorLayout />
+            </div>
+        )
+    }
+}
+
+const CalculatorLayout = () => 
+    <div className ="calculator-layout">
+        <Button onClick={() => console.log(1)}
+        className="button_1"
+        >
+            1
+        </Button>
+
+        <button 
+            onClick={() => console.log(2)}
+            className="button1"
+            >
+            2
+            </button>
+    </div>
+
+ const Button = ({onClick, className= '', children}) =>
+    <button
+        onClick={onClick}
+        className={className}
+        type="button"
+        >
+        {children}
+        </button>
 
 
 export default App; //The name of the class that extends to component that you'll be displaying
