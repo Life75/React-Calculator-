130d4bf8e4e7bd3a6f5ff91ae069f8fca9db0660
Life75 <austynwashington@yahoo.com>
2021-06-08 00:22:47 -0500

    styles updated

--- src/App.js
+++ src/App.js
@@ -2,32 +2,76 @@ import logo from "./logo.svg";
 import React, { Component } from "react";
 import styles from "./App.css";
 
+const window = {};
+
 class App extends Component {
   
     render() {
         return (
             <div className="page">
-            <CalculatorLayout />
-            </div>
-        )
+                <div className=''>
+                    <CalculatorLayout/>
+                </div>
+                <LayoutOne/>
+                </div>
+        );
     }
 }
 
-const CalculatorLayout = () => 
-    <div className ="calculator-layout">
+
+
+const CalculatorLayout = () =>
+        <div className ="button1">
+
+
         <Button onClick={() => console.log(1)}
-        className="button_1"
+        className="1"
         >
-            1
+            7
         </Button>
 
-        <button 
-            onClick={() => console.log(2)}
-            className="button1"
-            >
-            2
-            </button>
-    </div>
+        <Button onClick={() => console.log(2)}
+        className='2'
+        >
+            8
+        </Button>
+
+        <Button onClick={() => console.log(3)}
+        className='3'
+        >
+            9
+        </Button>
+
+        </div>
+
+
+
+const LayoutOne = () =>
+    <div className = 'layout2'>
+
+        <Button onClick={() => console.log(1)}
+        className="1"
+        >
+            5
+        </Button>
+
+        <Button onClick={() => console.log(2)}
+        className='2'
+        >
+            8
+        </Button>
+
+        <Button onClick={() => console.log(3)}
+        className='3'
+        >
+            9
+        </Button>
+
+    </div> 
+
+    
+
+ 
 
  const Button = ({onClick, className= '', children}) =>
     <button
@@ -37,6 +81,9 @@ const CalculatorLayout = () =>
         >
         {children}
         </button>
+
+
+
 
 
 export default App; //The name of the class that extends to component that you'll be displaying
